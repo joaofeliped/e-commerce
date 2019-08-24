968ba1f5cf12f4e560d95b9ef918b91d5a2a6679
João Felipe <jo_5919@hotmail.com>
2019-08-24 09:26:50 -0300

    Adds redux-saga to project

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -7,13 +7,13 @@ import * as CartActions from '../../store/modules/cart/actions';
 import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, total, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1);
+    updateAmountRequest(product.id, product.amount - 1);
   }
 
   function increment(product) {
-    updateAmount(product.id, product.amount + 1);
+    updateAmountRequest(product.id, product.amount + 1);
   }
 
    return (
@@ -29,7 +29,7 @@ function Cart({ cart, total, removeFromCart, updateAmount }) {
         </thead>
         <tbody>
             {cart.map(product => (
-              <tr>
+              <tr key={product.id}>
                 <td>
                   <img src={product.image} alt={product.title} />
                 </td>
